1cd4fbf38979660a4d9cb0bb11a81e0e073e3628
brunonasc74 <bruno.hpn74@gmail.com>
2022-09-12 02:49:03 -0300

    estrutura base da homepage criada

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Home from './pages/Home';
+
 function App() {
 	return (
 		<div>
-			<h1>App</h1>
+			<Home />
 		</div>
 	);
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import theme from '../../theme';
+import GlobalStyles from '../../components/styles/GlobalStyles';
+import Container from '../../components/styles/Container';
 
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<ThemeProvider theme={theme}>
+			<GlobalStyles />
+			<>
+				<Container>
+					<div>Home</div>
+				</Container>
+			</>
+		</ThemeProvider>
+	);
 };
 
 export default Home;
