d92d29ee801b21bac2c0f2e03efaa39e1ad48963
Shiellyn Bruna Ferreira <shiellynferrer@gmail.com>
2022-09-12 13:27:52 -0300

    adicionando texto inicial da página contato com animação

---
+++ src/pages/Contato/style.jsx
@@ -0,0 +1,92 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+p {
+  margin-top: 2rem;
+  width: 65%;
+  text-align: center;
+}
+`
+
+export const Box = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+gap: 2rem;
+`
+
+export const Animation = styled.div`
+
+ /* position: relative; */
+ width: 2.5em;
+ height: 2.5em;
+ transform: rotate(165deg);
+
+ &:before, &:after {
+ content: "";
+ position: absolute;
+ top: 50%;
+ left: 50%;
+ display: block;
+ width: 0.5em;
+ height: 0.5em;
+ border-radius: 0.25em;
+ transform: translate(-50%, -50%);
+}
+
+&:before {
+ animation: before8 2s infinite;
+}
+
+&:after {
+ animation: after6 2s infinite;
+}
+
+@keyframes before8 {
+ 0% {
+  width: 0.5em;
+  box-shadow: 1em -0.5em rgba(225, 20, 98, 0.75), -1em 0.5em rgba(111, 202, 220, 0.75);
+ }
+
+ 35% {
+  width: 2.5em;
+  box-shadow: 0 -0.5em rgba(225, 20, 98, 0.75), 0 0.5em rgba(111, 202, 220, 0.75);
+ }
+
+ 70% {
+  width: 0.5em;
+  box-shadow: -1em -0.5em rgba(225, 20, 98, 0.75), 1em 0.5em rgba(111, 202, 220, 0.75);
+ }
+
+ 100% {
+  box-shadow: 1em -0.5em rgba(225, 20, 98, 0.75), -1em 0.5em rgba(111, 202, 220, 0.75);
+ }
+}
+
+@keyframes after6 {
+ 0% {
+  height: 0.5em;
+  box-shadow: 0.5em 1em rgba(61, 184, 143, 0.75), -0.5em -1em rgba(233, 169, 32, 0.75);
+ }
+
+ 35% {
+  height: 2.5em;
+  box-shadow: 0.5em 0 rgba(61, 184, 143, 0.75), -0.5em 0 rgba(233, 169, 32, 0.75);
+ }
+
+ 70% {
+  height: 0.5em;
+  box-shadow: 0.5em -1em rgba(61, 184, 143, 0.75), -0.5em 1em rgba(233, 169, 32, 0.75);
+ }
+
+ 100% {
+  box-shadow: 0.5em 1em rgba(61, 184, 143, 0.75), -0.5em -1em rgba(233, 169, 32, 0.75);
+ }
+}
+
+`
