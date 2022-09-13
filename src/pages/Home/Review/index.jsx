1875c7c5e9b5fa37eb0981ca477f1354d62a39ad
brunonasc74 <bruno.hpn74@gmail.com>
2022-09-13 17:33:42 -0300

    Componente Review criado

---
+++ src/pages/Home/Review/index.jsx
@@ -0,0 +1,15 @@
+import Container from '../../../components/styles/Container';
+import { StyledSection } from './style';
+import Title from '../Title';
+
+const Review = () => {
+	return (
+		<StyledSection>
+			<Container>
+				<Title preTitle={'Depoimentos'} mainTitle={'O que os clientes dizem'} />
+			</Container>
+		</StyledSection>
+	);
+};
+
+export default Review;
