25b7e602230702931cb36d337b6ced73580a5086
brunonasc74 <bruno.hpn74@gmail.com>
2022-09-13 21:07:54 -0300

    componente CardReview criado

---
+++ src/pages/Home/CardReview/style.jsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+export const StyledCardReview = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	max-width: 500px;
+	min-height: 400px;
+	background-color: #0d0d14;
+	border-radius: 20px;
+	padding: 40px;
+	margin: 20px;
+	.flex {
+		img {
+			width: 6rem;
+			height: 6rem;
+			margin: 0 20px 10px 0;
+			border-radius: 50%;
+		}
+		display: flex;
+		justify-content: start;
+		align-items: center;
+		.title-flex {
+			display: flex;
+			flex-direction: column;
+			h3 {
+				margin-bottom: -1.8rem;
+			}
+			h4 {
+				color: #c5c5c5;
+				font-weight: normal;
+			}
+		}
+	}
+	p {
+		color: #c5c5c5;
+	}
+	.rating {
+		color: #ffd000;
+		* {
+			margin-right: 5px;
+		}
+	}
+`;
