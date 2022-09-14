8d34f4f983b7a6b83f28eec8f376cbd39b9e9799
Shiellyn Bruna Ferreira <shiellynferrer@gmail.com>
2022-09-14 13:05:19 -0300

    adicionando componente sectionteam que contem renderizado o componente cardteam

---
+++ src/pages/Contato/SectionTeam/style.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`
+
+export const Box = styled.div`
+` 
+export const BoxReverse = styled.div`
+`
