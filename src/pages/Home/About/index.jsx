ba1842bcc202c4562bcb723e7d23922b5ed74f17
Shiellyn Bruna Ferreira <shiellynferrer@gmail.com>
2022-09-15 20:33:50 -0300

    adicionando componente About

---
+++ src/pages/Home/About/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Title from "../../../components/Title";
+import { Container } from "./style";
+
+const About = () => {
+  return (
+    <>
+      <Container>
+        <h1>Por que escolher nosso resturante</h1>
+       <Title 
+       
+       />
+        <p>
+          Um dos restaurantes mais bem localizados da cidade, estacionamento
+          próprio, alimentos frescos e de qualidade.
+        </p>
+      </Container>
+    </>
+  );
+};
+
+export default About;
---
+++ src/pages/Home/About/style.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+display: flex;
+flex-direction: column;
+align-items: center;
+
+p {
+  width: 45%;
+  text-align: center;
+}
+`
