dff20c93aa4a24dbb7fb37e3d1a74223481dd4e9
Bruna Xavier <brunaxavier@id.uff.br>
2022-09-15 19:55:40 -0300

    feat: adição de arquivos de estilo do cardapio/card

--- src/pages/Cardapio/Card/index.jsx
+++ src/pages/Cardapio/Card/index.jsx
@@ -3,5 +3,23 @@ import theme from "../../theme";
 import GlobalStyles from "../../components/styles/GlobalStyles";
 import { StyledCard } from 'style';
 
-
+const Card = ({image, name, price, desc}) => {
+    return (
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <div className="card">
+            <div className="card-image">
+              <img src={image} alt="{name}" />
+            </div>
+            <div className="card-content">
+              <p className="card-name">{name}</p>
+              <p className="card-price">{price}</p>
+              <p className="card-desc">{desc}</p>
+          </div>
+        </div>
+      </ThemeProvider>
+    );
+  };
+  
+  export default Card;
 
---
+++ src/pages/Cardapio/style.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledCardapio = styled.div`
+
+
+
+
+
+`
