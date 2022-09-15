810f7b7f8ba2d5055c810155495ea309fcde5713
brunonasc74 <bruno.hpn74@gmail.com>
2022-09-15 20:39:31 -0300

    Title adicionado no componente About

--- src/pages/Home/About/index.jsx
+++ src/pages/Home/About/index.jsx
@@ -1,22 +1,17 @@
-import React from "react";
-import Title from "../../../components/Title";
-import { Container } from "./style";
+import Title from '../../../components/Title';
+import { Container } from './style';
 
 const About = () => {
-  return (
-    <>
-      <Container>
-        <h1>Por que escolher nosso resturante</h1>
-       <Title 
-       
-       />
-        <p>
-          Um dos restaurantes mais bem localizados da cidade, estacionamento
-          próprio, alimentos frescos e de qualidade.
-        </p>
-      </Container>
-    </>
-  );
+	return (
+		<Container>
+			<Title />
+			<h1>Por que escolher nosso resturante</h1>
+			<p>
+				Um dos restaurantes mais bem localizados da cidade, estacionamento
+				próprio, alimentos frescos e de qualidade.
+			</p>
+		</Container>
+	);
 };
 
 export default About;
