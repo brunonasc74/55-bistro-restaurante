da3da2c4f792eb7a86a1463532368ac5ac0087e5
Bruna Xavier <brunaxavier@id.uff.br>
2022-09-15 19:43:27 -0300

    feat: criação do card do Cardapio

---
+++ src/pages/Cardapio/Card/index.jsx
@@ -0,0 +1,7 @@
+import { ThemeProvider } from "styled-components";
+import theme from "../../theme";
+import GlobalStyles from "../../components/styles/GlobalStyles";
+import { StyledCard } from 'style';
+
+
+
