41b1a3fe5eafce1a8f01c2dabf6114082333d7ec
Shiellyn Bruna Ferreira <shiellynferrer@gmail.com>
2022-09-16 15:05:46 -0300

    adicionando componente CardAbout

---
+++ src/pages/Home/CardAbout/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Card } from './style';
+
+const CardAbout = () => {
+  return (
+    <Card>
+      <span>01</span>
+      <h4>Bem localizado</h4>
+      <p>Descrição do motivo por que seu restaurante
+        é o melhor.descrição do motivo por que seu restaurante
+        é o melhor.
+      </p>
+    </Card>
+  )
+}
+
+export default CardAbout;
---
+++ src/pages/Home/CardAbout/style.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+width: 400px;
+height: 280px;
+border-radius: 10px;
+padding: 1rem;
+background-color: #0d0d14;
+display: flex;
+flex-direction: column;
+justify-content: center;
+
+span {
+  font-size: 30px;
+  font-weight: bold;
+  color: #ff3a39;
+}
+
+h4 {
+  font-size: 22px;
+}
+
+ p {
+  text-align: left;
+  font-size: 16px;
+ }
+`
