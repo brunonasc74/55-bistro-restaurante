50c8f761da05625ace1173357f98dc1df4fef7eb
Shiellyn Bruna Ferreira <shiellynferrer@gmail.com>
2022-09-16 16:10:40 -0300

    resolvendo conflito

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,11 +2,7 @@ import { ThemeProvider } from 'styled-components';
 import theme from '../../theme';
 import GlobalStyles from '../../components/styles/GlobalStyles';
 import Header from './Header';
-<<<<<<< HEAD
-import About from './About'
-=======
 import About from './About';
->>>>>>> 7bb59f9d02588252c0ad10c0a9cedc22da43c76e
 import Review from './Review';
 import Event from './Event';
 import Recipe from './Recipe';
@@ -18,11 +14,7 @@ const Home = () => {
 			<GlobalStyles />
 			<>
 				<Header />
-<<<<<<< HEAD
-			  <About />
-=======
 				<About />
->>>>>>> 7bb59f9d02588252c0ad10c0a9cedc22da43c76e
 				<Review />
 				<Event />
 				<Recipe />
