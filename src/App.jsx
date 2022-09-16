c528a7c8e8fa68f6cdc826add583d5d8e855f044
brunonasc74 <bruno.hpn74@gmail.com>
2022-09-16 20:32:23 -0300

    remoção de código em App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import Contato from './pages/Contato';
-// import CardContato from './pages/Contato/Card';
-import SectionTeam from './pages/Contato/SectionTeam';
-import CardTeamReverse from './pages/Contato/SectionTeam/CardTeam/cardTeamReverse';
-import Home from './pages/Home'
-import About from './pages/Home/About';
-// import SectionTime from './pages/Contato/SectionTime';
+import Home from './pages/Home';
 
 function App() {
 	return (
- <div>		
-			<Contato/>
-			{/* <CardContato/> */}
-			{/* <SectionTime/> */}
-			{/* <SectionTeam/> */}
+		<div>
 			<Home />
-			
 		</div>
 	);
 }
