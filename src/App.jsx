c4e601ec0782067b0d17610e651abacf1545b7d3
Bruna Xavier <brunaxavier@id.uff.br>
2022-09-17 16:26:30 -0300

    fix: conflito de merge resolvido

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
+import Home from './pages/Home';
 import Cardapio from './pages/Cardapio';
 
-=======
-
-import Footer from './components/Footer';
-import Nav from './components/Nav';
-import Home from './pages/Home';
->>>>>>> f002ceb49332b1e6dc1015f8e7d2723564061945
 
 function App() {
 	return (
