aad08b6942ebc810d7a46ffd24edd1058dfb0ad8
Bruna Xavier <brunaxavier@id.uff.br>
2022-09-21 12:33:33 -0300

    fix: alinhando botao

--- src/pages/Cardapio/Button/style.jsx
+++ src/pages/Cardapio/Button/style.jsx
@@ -12,7 +12,7 @@ const Button = styled.div`
     font-size: 16px;
     font-weight: 700;
     cursor: pointer;
-    align-items: center;
+    text-align: center;
     justify-content: center;
 
     &:hover {
